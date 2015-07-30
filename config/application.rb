faa1263a99dc202dfaa2fa1817bd7d9e70cfa6ec
brendenmartin05 <brendenmartin1987@yahoo.com>
2015-07-30 12:53:37 -0700

    testing for heroku

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module Picpatter
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.assets.enabled=true
-    config.assets.initialize_on_precompile = false
+    # config.assets.enabled=true
+
   end
 end
